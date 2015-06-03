865cae451a83515186f47a067c630aabf950d385
jnzheng <jnzheng@thoughtworks.com>
2015-06-03 16:32:00 +0800

    add MyBooksCollectionViewController

---
+++ TWJieShuQuan/Profile/MyBooksCollectionViewController.h
@@ -0,0 +1,13 @@
+//
+//  MyBooksCollectionViewController.h
+//  TWJieShuQuan
+//
+//  Created by Jianning Zheng on 6/3/15.
+//  Copyright (c) 2015 Jianing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MyBooksCollectionViewController : UICollectionViewController
+
+@end
