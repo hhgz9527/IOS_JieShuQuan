a641a6637384e0ca9812f0d1eba14ca077f27a9f
jnzheng <jnzheng@thoughtworks.com>
2015-06-05 16:35:20 +0800

    add image shadow

--- TWJieShuQuan/Profile/MyBooksCollectionViewCell.m
+++ TWJieShuQuan/Profile/MyBooksCollectionViewCell.m
@@ -8,12 +8,26 @@
 
 #import "MyBooksCollectionViewCell.h"
 #import <SDWebImage/UIImageView+WebCache.h>
+#import "QuartzCore/CALayer.h"
 
 @implementation MyBooksCollectionViewCell
+
+- (void)awakeFromNib {
+    [self applyShadowOn:self.bookImageView];
+}
 
 - (void)refreshCellWithBookEntity:(BookEntity *)bookEntity {
     self.bookNameLabel.text = bookEntity.bookName;
     [self.bookImageView sd_setImageWithURL:[NSURL URLWithString:bookEntity.bookImageHref]];
+}
+
+#pragma mark - private
+
+- (void)applyShadowOn:(UIImageView *)someImageView {
+    someImageView.layer.shadowColor = [UIColor blackColor].CGColor;
+    someImageView.layer.shadowOffset = CGSizeMake(0, 2);
+    someImageView.layer.shadowOpacity = 0.5;
+    someImageView.clipsToBounds = NO;
 }
 
 @end
