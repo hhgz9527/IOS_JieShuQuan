c4370d0ccab5ea0bceb66bb361a7681e788f0f3b
GaoYu <hhgz9527@gmail.com>
2015-06-27 21:13:54 +0800

    build select office flow

---
+++ TWJieShuQuan/Profile/OfficeTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  OfficeTableViewController.h
+//  TWJieShuQuan
+//
+//  Created by GaoYu on 15/6/27.
+//  Copyright (c) 2015年 Jianing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OfficeTableViewController : UITableViewController
+
+@end
--- TWJieShuQuan/Profile/SendTwitterViewController.m
+++ TWJieShuQuan/Profile/SendTwitterViewController.m
@@ -10,6 +10,7 @@
 #import <AVObject.h>
 #import <AVUser.h>
 #import "CustomAlert.h"
+#import "OfficeTableViewController.h"
 
 @interface SendTwitterViewController ()
 
@@ -24,6 +25,7 @@
     // Do any additional setup after loading the view.
     self.title = @"发信息";
     [_textView becomeFirstResponder];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardDidShow:) name:UIKeyboardDidShowNotification object:nil];
 }
 
 - (IBAction)dismiss:(id)sender {
@@ -44,6 +46,30 @@
     } else {
         [[CustomAlert alloc] showAlertWithMessage:@"字数限制"];
     }
+}
+
+- (void)keyboardDidShow:(NSNotification *)nofi {
+    NSDictionary *dic = [nofi userInfo];
+    NSValue *value = [dic objectForKey:@"UIKeyboardFrameEndUserInfoKey"];
+    CGRect rect = [value CGRectValue];
+
+    UIView *view = [[UIView alloc] initWithFrame:CGRectMake(0, rect.origin.y - 44, rect.size.width, 44)];
+    view.backgroundColor = [UIColor colorWithRed:243.f/255.f green:243.f/255.f blue:243.f/255.f alpha:1];
+    UIButton *btn = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    btn.frame = CGRectMake(0, 0, 44, 44);
+    btn.center = CGPointMake(20, 22);
+    [btn setTitle:@"@" forState:UIControlStateNormal];
+    btn.titleLabel.font = [UIFont systemFontOfSize:25];
+    [btn addTarget:self action:@selector(selectNotification) forControlEvents:UIControlEventTouchUpInside];
+    [view addSubview:btn];
+    
+    [self.view addSubview:view];
+}
+
+- (void)selectNotification {
+    OfficeTableViewController *office = [[OfficeTableViewController alloc] init];
+    UINavigationController *nav = [[UINavigationController alloc] initWithRootViewController:office];
+    [self.navigationController presentViewController:nav animated:YES completion:nil];
 }
 
 @end
