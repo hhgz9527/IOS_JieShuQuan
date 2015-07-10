284880a08f2751a41920a330b053b65ed152597b
jnzheng <jnzheng@thoughtworks.com>
2015-07-10 13:17:12 +0800

    fix upload avatar issue

--- TWJieShuQuan/Profile/ProfileTableViewController.m
+++ TWJieShuQuan/Profile/ProfileTableViewController.m
@@ -116,48 +116,75 @@ static NSInteger const kSetAvatarTag = 1001;
 
 }
 
+#pragma mark - UIImagePickerController Delegate
 
-- (void) imagePickerController: (UIImagePickerController*) reader didFinishPickingMediaWithInfo: (NSDictionary*) info
+- (void) imagePickerController: (UIImagePickerController*) picker didFinishPickingMediaWithInfo: (NSDictionary*) info
 {
-    id<NSFastEnumeration> results = [info objectForKey: ZBarReaderControllerResults];
-    ZBarSymbol *symbol = nil;
-    for(symbol in results) {
-        // EXAMPLE: just grab the first barcode
-        break;
-    }
-    
-    NSString *barCode = symbol.data;
-    NSLog(@"---------- barcode:%@", barCode);
-    
-    if (barCode) {
-        [[CustomActivityIndicator sharedActivityIndicator] startAsynchAnimating];
-        [DouBanService fetchingBookDetailWithISBN:barCode succeeded:^(NSDictionary *bookObject){
-            [[CustomActivityIndicator sharedActivityIndicator] stopAsynchAnimating];
-            
-            NSLog(@"isbn succeed......%@", bookObject);
-            
-            Book *book = [[Book alloc] init];
-            book.bookDoubanId = [bookObject valueForKey:@"id"];
-            book.bookName = [bookObject valueForKey:@"title"];
-            book.bookAuthor = [[bookObject valueForKey:@"author"] componentsJoinedByString:@","];
-            book.bookImageHref = [bookObject valueForKey:@"image"];
-            book.bookPress = bookObject[@"publisher"];
-            book.bookDescription = bookObject[@"summary"];
-            
-            AddToLibraryViewController *addToLibraryVC = [[AddToLibraryViewController alloc] initWithNibName:@"AddToLibraryViewController" bundle:nil];
-            addToLibraryVC.delegate = self;
-            addToLibraryVC.book = book;
-            [self presentViewController:addToLibraryVC animated:YES completion:nil];
-        } failed:^{
-            [[CustomActivityIndicator sharedActivityIndicator] stopAsynchAnimating];
-            
-            NSLog(@"isbn fail......");
-        }];
+    NSString *type = info[UIImagePickerControllerMediaType];
+    if ([type isEqualToString:@"public.image"]) {
+        // 上传头像
+        UIImage *image = info[UIImagePickerControllerEditedImage];
+        
+        NSData *data = nil;
+        if (UIImagePNGRepresentation(image)) {
+            data = UIImagePNGRepresentation(image);
+        } else {
+            data = UIImageJPEGRepresentation(image, 0.8f);
+        }
+        NSString * DocumentsPath = [NSHomeDirectory() stringByAppendingPathComponent:@"Documents"];
+        
+        NSFileManager *fileManager = [NSFileManager defaultManager];
+        
+        [fileManager createDirectoryAtPath:DocumentsPath withIntermediateDirectories:YES attributes:nil error:nil];
+        [fileManager createFileAtPath:[DocumentsPath stringByAppendingString:@"/image.png"] contents:data attributes:nil];
+        
+        [picker dismissViewControllerAnimated:YES completion:nil];
+        
+        self.avatar.image = image;
+        
+        [self uploadAvatar:data];
     } else {
-        [[CustomAlert sharedAlert] showAlertWithMessage:@"获取图书信息失败"];
+        // 扫码入库
+        id<NSFastEnumeration> results = [info objectForKey: ZBarReaderControllerResults];
+        ZBarSymbol *symbol = nil;
+        for(symbol in results) {
+            // EXAMPLE: just grab the first barcode
+            break;
+        }
+        
+        NSString *barCode = symbol.data;
+        NSLog(@"---------- barcode:%@", barCode);
+        
+        if (barCode) {
+            [[CustomActivityIndicator sharedActivityIndicator] startAsynchAnimating];
+            [DouBanService fetchingBookDetailWithISBN:barCode succeeded:^(NSDictionary *bookObject){
+                [[CustomActivityIndicator sharedActivityIndicator] stopAsynchAnimating];
+                
+                NSLog(@"isbn succeed......%@", bookObject);
+                
+                Book *book = [[Book alloc] init];
+                book.bookDoubanId = [bookObject valueForKey:@"id"];
+                book.bookName = [bookObject valueForKey:@"title"];
+                book.bookAuthor = [[bookObject valueForKey:@"author"] componentsJoinedByString:@","];
+                book.bookImageHref = [bookObject valueForKey:@"image"];
+                book.bookPress = bookObject[@"publisher"];
+                book.bookDescription = bookObject[@"summary"];
+                
+                AddToLibraryViewController *addToLibraryVC = [[AddToLibraryViewController alloc] initWithNibName:@"AddToLibraryViewController" bundle:nil];
+                addToLibraryVC.delegate = self;
+                addToLibraryVC.book = book;
+                [self presentViewController:addToLibraryVC animated:YES completion:nil];
+            } failed:^{
+                [[CustomActivityIndicator sharedActivityIndicator] stopAsynchAnimating];
+                
+                NSLog(@"isbn fail......");
+            }];
+        } else {
+            [[CustomAlert sharedAlert] showAlertWithMessage:@"获取图书信息失败"];
+        }
+        
+        [picker dismissViewControllerAnimated:YES completion:nil];
     }
-    
-    [reader dismissViewControllerAnimated:YES completion:nil];
 }
 
 #pragma mark <AddToLibraryDelegate>
